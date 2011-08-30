07cb7efa934e241fe061202872fa5ef795640784
Justin Dearing <zippy1981@gmail.com>
2011-08-30 08:19:53 -0400

    This is now a series of unit tests.

--- xml2json/Main.cs
+++ xml2json/Main.cs
@@ -1,28 +1,75 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
-
+using NUnit.Framework;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Bson;
 using JsonFormatting = Newtonsoft.Json.Formatting;
 
 namespace xml2json
 {
+    [TestFixture]
 	class MainClass
 	{
+        public MainClass() {}
+
+        private static readonly MainClass instance;
+
+        internal static MainClass Instance
+        {
+            get { return instance; }
+        }
+
+        static MainClass()
+        {
+            instance = new MainClass();
+        }
+
 		/// <summary>
-		/// Convert the NYS Thruway authority Park ANd Ride xml file to json.
+		/// Convert the NYS Thruway authority Park and Ride xml file to json.
 		/// </summary>
 		/// <param name="args">Command line arguments.</param>
 		public static void Main (string[] args)
 		{
-			var xml = new XmlDocument();
-			//xml.Load("parkridelots.xml");
-			xml.Load("testcase.xml");
-			var json = JsonConvert.SerializeXmlNode(xml, JsonFormatting.Indented);
-			File.WriteAllText("testcase.json", json);
-			Console.WriteLine(json);
+			Instance.TestTestCase();
 		}
+
+        private static string XmlFileToJson(string filename)
+        {
+            var xml = new XmlDocument();
+            xml.Load(filename);
+            return JsonConvert.SerializeXmlNode(xml);
+        }
+
+        [Test]
+        public void TestParkAndRides()
+        {
+            Console.WriteLine("JSON: {0}", XmlFileToJson("parkridelots.xml"));
+        }
+
+        [Test]
+        public void TestTestCase()
+        {
+            Console.WriteLine("JSON: {0}", XmlFileToJson("testcase.xml"));
+        }
+
+
+
+        [Test]
+        public void TestTestCaseDtd()
+        {
+            Console.WriteLine("JSON: {0}", XmlFileToJson("testcase.dtd"));
+        }
+
+        [Test]
+        public void TestHtml401Dtd()
+        {
+            //Console.WriteLine("JSON: {0}", XmlFileToJson("html4.0.1-strict.dtd"));
+            Console.WriteLine("JSON: {0}", XmlFileToJson("xmlspec.dtd"));
+        }
 	}
 }
 
